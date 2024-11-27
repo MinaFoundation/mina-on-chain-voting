64b0905bbc93c6314247b5bce64746895446c405
manza <leomanzanal@gmail.com>
2024-11-27 07:11:32 -0300

    On-Chain Voting for MIP - Granola UI update (#25)

--- web/components/theme-toggle.tsx
+++ web/components/theme-toggle.tsx
@@ -22,8 +22,8 @@ export const ThemeToggle = () => {
 
   return (
     <Button variant="ghost" size="icon" onClick={switchTheme}>
-      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <SunIcon className="text-white hover:text-foreground/80 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="text-white hover:text-foreground/80 absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">{theme === 'light' ? 'Light mode' : 'Dark mode'}</span>
     </Button>
   );
