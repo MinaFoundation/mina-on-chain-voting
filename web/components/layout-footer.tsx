239129274762c561f0d2ba0c2a9e1dcea1d27766
manza <leomanzanal@gmail.com>
2024-12-02 06:17:29 -0300

    Minor UI fixes: archor colors on light mode + open link other tab (#31)

--- web/components/layout-footer.tsx
+++ web/components/layout-footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
         {/* Main Footer Content */}
         <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start space-y-4 lg:space-y-0">
           {/* Copyright Text */}
-          <p className="text-gray-400 text-sm">© 2024 Mina Foundation. All rights reserved.</p>
+          <p className="text-gray-500 text-sm">© 2024 Mina Foundation. All rights reserved.</p>
 
           {/* Links */}
           <div className="flex flex-col lg:flex-row items-center space-y-2 lg:space-y-0 lg:space-x-6">
@@ -15,7 +15,7 @@ export const Footer = () => {
               href="https://github.com/MinaProtocol/mina/blob/develop/CODE_OF_CONDUCT.md"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-gray-400 text-sm hover:text-white"
+              className="text-gray-500 text-sm dark:hover:text-white hover:text-gray-400"
             >
               Code of Conduct
             </a>
@@ -23,7 +23,7 @@ export const Footer = () => {
               href="https://minaprotocol.com/privacy"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-gray-400 text-sm hover:text-white"
+              className="text-gray-500 text-sm dark:hover:text-white hover:text-gray-400"
             >
               Privacy Policy
             </a>
@@ -31,7 +31,7 @@ export const Footer = () => {
               href="https://minaprotocol.com/tos"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-gray-400 text-sm hover:text-white"
+              className="text-gray-500 text-sm dark:hover:text-white hover:text-gray-400"
             >
               Terms of Service
             </a>
@@ -39,7 +39,7 @@ export const Footer = () => {
               href="https://minaprotocol.com/impressum"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-gray-400 text-sm hover:text-white"
+              className="text-gray-500 text-sm dark:hover:text-white hover:text-gray-400"
             >
               Impressum
             </a>
@@ -47,7 +47,7 @@ export const Footer = () => {
         </div>
 
         {/* Built by Granola */}
-        <div className="mt-4 text-gray-400 text-sm text-center lg:text-left">
+        <div className="mt-4 text-gray-500 text-sm text-center lg:text-left">
           Built by{' '}
           <a
             href={siteConfig.links.granola}
