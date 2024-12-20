6e46db7aeb3456f31fb164ae8f2ff61b266fe5d1
leomanza <leomanzanal@gmail.com>
2024-12-20 10:09:06 -0300

    ranked vote wip

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -6,6 +6,10 @@ mod proposals;
 mod serve;
 mod util;
 mod vote;
+mod ranked_vote_builder;
+mod ranked_vote_config;
+mod ranked_vote;
+
 
 pub use archive::*;
 pub use config::*;
@@ -15,3 +19,7 @@ pub use proposals::*;
 pub use serve::*;
 pub use util::*;
 pub use vote::*;
+pub use ranked_vote_config::*;
+pub use ranked_vote_builder::*;
+pub use ranked_vote::*;
+
